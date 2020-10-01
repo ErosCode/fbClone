5c94b9b9978eb5f4bb2f167cdf322ed86b50862d
ErosCode <corentin.dc.eros@gmail.com>
2020-10-02 01:56:16 +0200

    add image storage

--- fb-clone-backend/server.js
+++ fb-clone-backend/server.js
@@ -7,21 +7,78 @@ import Grid from 'gridfs-stream';
 import bodyParser from 'body-parser';
 import path from 'path';
 import Pusher from 'pusher';
-
+import mongoPost from './Models/post';
+// import my database connection url by an hidden file
+import { DB_CONNECT } from './dbInfos'
 Grid.mongo = mongoose.mongo;
 
 // App Config
 const app = express();
-const port = process.env.PORT || 3002
+const port = process.env.PORT || 9000;
 
 //middlewares
 app.use(bodyParser.json());
 app.use(cors());
 
 // db config
+const mongoURI = DB_CONNECT;
+const conn = mongoose.createConnection(mongoURI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+let gfs
+
+conn.once('open', () => {
+    console.log('DB Connected')
+    gfs = Grid(conn.db, mongoose.mongo)
+    gfs.collection('images');
+});
+
+const storage = new GridFsStorage({
+    url: mongoURI,
+    file: (req, file) => {
+        return new Promise((resolve, reject) => {
+            const filename = `image-${Date.now()}${path.extname(file.originalname)}`
+            const fileInfo = {
+                filename: filename,
+                bucketName: 'images'
+            };
+            resolve(fileInfo);
+        });
+    }
+});
+
+const upload = multer({storage});
+
+mongoose.connect(mongoURI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 // api routes
-app.get('/', (req, res) => res.status(200).send('hello world'))
+app.get('/', (req, res) => res.status(200).send('hello world'));
+
+app.post('/upload/image', upload.single('file'), (req, res) => {
+    res.status(201).send(req.file);
+});
+
+app.post('/upload/post', (req, res) => {
+    const dbPost = reqbody;
+    mongoPost.create(dbPost, (err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(201).send(data)
+        }
+    })
+})
+
+app.get('/retrieve/posts', (req, res) => {
+
+})
 
 // listen
-app.listen(port, ()=>console.log(`listening on localhost: ${port}`));
+app.listen(port, () => console.log(`listening on localhost: ${port}`));
