8ad8af68d787351b00063f7d67d8c4d7011d63b6
ErosCode <corentin.dc.eros@gmail.com>
2021-03-01 19:29:34 +0100

    start

--- fb-clone-frontend/src/axios.js
+++ fb-clone-frontend/src/axios.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'https://fb-clone-heroku.herokuapp.com/'
+    baseURL: 'http://localhost:9000'
 })
 
-export default instance;
+export default instance
