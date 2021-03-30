8122f71e31e18b8da1450ffbe8994d683042d1f3
ErosCode <corentin.dc.eros@gmail.com>
2021-03-30 15:59:28 +0200

    firebase config + widget changes

---
+++ fb-clone-frontend/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDTUldi4J1d4B3Od8zE8CDafyAGvMnnrKM",
+    authDomain: "fb-clone-c13c8.firebaseapp.com",
+    databaseURL: "https://fb-clone-c13c8.firebaseio.com",
+    projectId: "fb-clone-c13c8",
+    storageBucket: "fb-clone-c13c8.appspot.com",
+    messagingSenderId: "431932635681",
+    appId: "1:431932635681:web:f74bc6a646a25f57d54160"
+  };
