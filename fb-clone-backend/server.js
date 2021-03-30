76cd1dcd89ad0e577aa2df269f5fbf8e46711a13
ErosCode <corentin.dc.eros@gmail.com>
2021-03-30 16:47:49 +0200

    add new route for headers

--- fb-clone-backend/server.js
+++ fb-clone-backend/server.js
@@ -139,5 +139,11 @@ app.get("/retrieve/posts", (req, res) =>{
     })
 })
 
+app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+  });
+
 //listen
 app.listen(PORT, () => console.log(`listening on localhost:${PORT}`));
